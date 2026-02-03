e5b234db54191e0e3beccdc7d32e0aef0d78f0b8
D1GQ <yourboylax@gmail.com>
2026-02-03 13:49:20 -0600

    move to hotfix 2

--- src/ModInfo.cs
+++ src/ModInfo.cs
@@ -31,7 +31,7 @@ internal static class ModInfo
     /// <summary>
     /// The hotfix number for hotfix releases.
     /// </summary>
-    internal const string HOTFIX_NUM = "1";
+    internal const string HOTFIX_NUM = "2";
 
     /// <summary>
     /// Indicates whether this is a hotfix release.
