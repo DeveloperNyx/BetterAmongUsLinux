275cb6c98ea443d289981c033189ec9bf42c8882
D1GQ <yourboylax@gmail.com>
2026-03-18 15:16:14 -0500

    Show anonymous votes when dead and not Guardian Angel

---
+++ src/Patches/Gameplay/Options/LogicOptionsPatch.cs
@@ -0,0 +1,23 @@
+﻿using AmongUs.GameOptions;
+using BetterAmongUs.Helpers;
+using HarmonyLib;
+
+namespace BetterAmongUs.Patches.Gameplay.Options;
+
+[HarmonyPatch]
+internal static class LogicOptionsPatch
+{
+    [HarmonyPatch(typeof(LogicOptionsNormal), nameof(LogicOptionsNormal.GetAnonymousVotes))]
+    [HarmonyPostfix]
+    private static void LogicOptionsNormal_Update_Postfix(MeetingHud __instance, ref bool __result)
+    {
+        if (PlayerControl.LocalPlayer == null)
+            return;
+
+        // Show anonymous votes when dead and not Guardian Angel
+        if (!PlayerControl.LocalPlayer.IsAlive() && !PlayerControl.LocalPlayer.Is(RoleTypes.GuardianAngel))
+        {
+            __result = false;
+        }
+    }
+}
